7f7a1a8626598f5f6c406d21bc761159f7008779
Denny Wu <denny.wu13@gmail.com>
2012-05-15 14:45:20 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="/meh-hil/css/site.css" rel="stylesheet" type="css/text"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+	<script src="/meh-hil/javascript/plugin/slides.min.jquery.js"></script>
+        <script src="/meh-hil/javascript/site.js"></script>
+        <link rel="stylesheet" href="/meh-hil/css/slider.css">
+        <title></title>
+    </head>
+    <body>
+        <?php include_once 'views/navigation.php'; ?>
+        <div class="container-page">
+        <div id="slider">
+			<img src="/meh-hil/images/home/new-ribbon.png" width="112" height="112" alt="New Ribbon" id="ribbon">
+			<div id="slides">
+                    <div class="slides_container">
+                        <div class="slide">
+                        <a href="#" title="145.365 - Happy Bokeh Thursday! | Flickr - Photo Sharing!" target="_blank"><img src="/meh-hil/images/home/slide-1.jpg" width="570" height="270" alt="Slide 1"></a>
+                            <div class="caption" style="bottom:0">
+                                <p>Happy Bokeh Thursday!</p>
+                            </div>
+                        </div>
+                    <div class="slide">
+                        <a href="#" title="Taxi | Flickr - Photo Sharing!" target="_blank"><img src="/meh-hil/images/home/slide-2.jpg" width="570" height="270" alt="Slide 2"></a>
+                        <div class="caption">
+                            <p>Taxi</p>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <a href="#" title="Happy Bokeh raining Day | Flickr - Photo Sharing!" target="_blank"><img src="/meh-hil/images/home/slide-3.jpg" width="570" height="270" alt="Slide 3"></a>
+                        <div class="caption">
+                            <p>Happy Bokeh raining Day</p>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <a href="#" title="We Eat Light | Flickr - Photo Sharing!" target="_blank"><img src="/meh-hil/images/home/slide-4.jpg" width="570" height="270" alt="Slide 4"></a>
+                        <div class="caption">
+                            <p>We Eat Light</p>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <a href="#" title="&ldquo;I must go down to the sea again, to the lonely sea and the sky; and all I ask is a tall ship and a star to steer her by.&rdquo; | Flickr - Photo Sharing!" target="_blank"><img src="/meh-hil/images/home/slide-5.jpg" width="570" height="270" alt="Slide 5"></a>
+                        <div class="caption">
+                            <p>&ldquo;I must go down to the sea again, to the lonely sea and the sky...&rdquo;</p>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <a href="#" title="twelve.inch | Flickr - Photo Sharing!" target="_blank"><img src="/meh-hil/images/home/slide-6.jpg" width="570" height="270" alt="Slide 6"></a>
+                        <div class="caption">
+                            <p>twelve.inch</p>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <a href="#" title="Save my love for loneliness | Flickr - Photo Sharing!" target="_blank"><img src="/meh-hil/images/home/slide-7.jpg" width="570" height="270" alt="Slide 7"></a>
+                        <div class="caption">
+                            <p>Save my love for loneliness</p>
+                        </div>
+                    </div>
+				</div>
+				<a href="#" class="prev"><img src="/meh-hil/images/home/arrow-prev.png" width="24" height="43" alt="Arrow Prev"></a>
+				<a href="#" class="next"><img src="/meh-hil/images/home/arrow-next.png" width="24" height="43" alt="Arrow Next"></a>
+			</div>
+			<img src="/meh-hil/images/home/example-frame.png" width="739" height="341" alt="Example Frame" id="frame">
+        </div>
+        </div>
+		<?php include_once 'views/footer.php'; ?>
+    </body>
+</html>
---
+++ views/navigation.php
@@ -0,0 +1,35 @@
+<link href="/meh-hil/css/navigation.css" rel="stylesheet" type="css/text"/>
+<nav id="globalheader" class='apple globalheader-js noinset svg globalheader-loaded globalheader-loaded'>
+			<ul id="globalnav" role="navigation">
+				<li id="gn-store">
+					<a href="/meh-hil/index.php">
+						<span>Beranda</span>
+					</a>
+				</li>
+				<li>
+					<a>
+						<span>Buku</span>
+					</a>
+				</li>
+				<li>
+					<a>
+						<span>Pemesanan</span>
+					</a>
+				</li>
+				<li>
+					<a>
+						<span>Area Member</span>
+					</a>
+				</li>
+				<li>
+					<a>
+						<span>Tentang Kami</span>
+					</a>
+				</li>
+				<li style="width:100%;text-align:right;padding-right:5px;">
+					<div>
+						<input id="searchItem" type="text" placeholder="Cari" style="padding-left:5px;-moz-border-radius: 15px;border-radius: 15px;"/>
+					<div>
+				</li>
+			</ul>
+		</nav>
